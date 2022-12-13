d347a4f18d3f13991af97a080e5f252c7bb49949
Andrey <104010582+AshenRagee@users.noreply.github.com>
2022-12-13 17:09:43 +0300

    Add new Landing page - Xiaomi Himo C26

---
+++ Xiaomi Himo C26 Landing page/js/script.js
@@ -0,0 +1,37 @@
+$(function () {
+	$('.bike__slider, .slider__items').slick({
+		arrows: false,
+		dots: true,
+		fade: true,
+		autoplay: true,
+		autoplaySpeed: 3000,
+	});
+});
+
+// SMOOTH SCROLL, but can use css: scroll-behavior: smooth
+$(document).ready(function () {
+	// Добавить плавную прокрутку до всех ссылок
+	$('a').on('click', function (event) {
+		// Убедись в этом что .hash имеет значение перед переопределением поведения по умолчанию
+		if (this.hash !== '') {
+			// Запретить поведение щелчка якоря по умолчанию
+			event.preventDefault();
+
+			// Хранить хэш
+			var hash = this.hash;
+
+			// Использование метода animate() jQuery для добавления плавной прокрутки страницы
+			// Необязательное число (800) указывает количество миллисекунд, необходимых для прокрутки до указанной области
+			$('html, body').animate(
+				{
+					scrollTop: $(hash).offset().top,
+				},
+				800,
+				function () {
+					// Добавить хэш (#) для URL-адреса после завершения прокрутки (поведение щелчка по умолчанию)
+					window.location.hash = hash;
+				}
+			);
+		} // Конец, если
+	});
+});
